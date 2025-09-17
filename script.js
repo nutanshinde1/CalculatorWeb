d6a0353b5912e21feff61d02b9da424967f929af
Nutan Arjun Shinde <shindenutan3714@gmail.com>
2025-09-17 21:07:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let display = document.getElementById("display");
 function appendValue(value) {
     display.value += value;
 }  
-function clearDisplay() { 
+function clearDisplay() {  
     display.value = "";
 }  
 function calculateResult() {
